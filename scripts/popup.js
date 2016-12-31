0933f7eeed6411913f1edd0568e01cb44873ccb6
Mohamed Mansour <hello@mohamedmansour.com>
2016-12-31 15:07:42 -0800

    Hookup click event for see all ads.

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,15 +1,24 @@
 var domBlockedList = document.querySelector('#blocked-list')
 var domTotalAds = document.querySelector('#total-ads')
 var domSeeAllAds = document.querySelector('#see-all')
+var domSeeAllAdsList = document.querySelector('#see-all-list')
 
-chrome.tabs.query({ active: true, currentWindow: true}, function(tabs) {
+chrome.tabs.query({ active: true, currentWindow: true}, onCurrentTabFound)
+
+function onCurrentTabFound(tabs) {
     if (!tabs.length)
         return
 
     chrome.tabs.sendMessage(tabs[0].id, {type: 'BlockedAds'}, function(response) {
+        domSeeAllAds.addEventListener('click', onSeeAllClicked, false)
         renderBlockedAds(response.data)
     })
-})
+}
+
+function onSeeAllClicked() {
+    domSeeAllAds.style.display = 'none'
+    domSeeAllAdsList.style.display = 'block'
+}
 
 function renderBlockedAds(blockedAds) {
     domTotalAds.innerText = blockedAds.length
